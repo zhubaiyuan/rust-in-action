4e3774a0b1a91005cd1413362464502398b86dc9
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-25 04:07:03 +0800

    printing the line number along with the match

--- p20-grep-lite/src/main.rs
+++ p20-grep-lite/src/main.rs
@@ -5,9 +5,11 @@ Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of pages?";
+    let mut line_num: usize = 1;
     for line in quote.lines() {
         if line.contains(search_term) {
-            println!("{}", line);
+            println!("{}: {}", line_num, line);
         }
+        line_num += 1;
     }
 }
