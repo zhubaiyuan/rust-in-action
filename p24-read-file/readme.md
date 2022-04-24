64666e2687a664ee27aba8933b845b86c8b561a5
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-25 05:11:54 +0800

    reading a file line by line via BufReader::lines()

--- p24-read-file/readme.md
+++ p24-read-file/readme.md
@@ -1,1 +1,3 @@
-You must have a map, no matter how rough. Otherwise you wander all over the place.
+You must have a map,
+no matter how rough.
+Otherwise you wander all over the place.
--- p24-read-file/src/main.rs
+++ p24-read-file/src/main.rs
@@ -4,14 +4,9 @@ use std::io::BufReader;
 
 fn main() {
     let f = File::open("readme.md").unwrap();
-    let mut reader = BufReader::new(f);
-    let mut line = String::new();
-    loop {
-        let len = reader.read_line(&mut line).unwrap();
-        if len == 0 {
-            break;
-        }
-        println!("{} ({} bytes long)", line, len);
-        line.truncate(0);
+    let reader = BufReader::new(f);
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        println!("{} ({} bytes long)", line, line.len());
     }
 }
