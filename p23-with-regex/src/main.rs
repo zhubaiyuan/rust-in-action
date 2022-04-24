1b1e139b7331a7e5f28a3df544a399d6d5cdb27f
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-25 05:03:25 +0800

    supporting command-line arguments

--- p23-with-regex/src/main.rs
+++ p23-with-regex/src/main.rs
@@ -1,15 +1,26 @@
+use clap::{Arg, Command};
 use regex::Regex;
 
 fn main() {
-    let re = Regex::new("picture").unwrap();
+    let args = Command::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::new("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
     let quote = "\
 Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of pages?";
     for line in quote.lines() {
-        let contains_substring = re.find(line);
-        match contains_substring {
+        match re.find(line) {
             Some(_) => println!("{}", line),
             None => (),
         }
