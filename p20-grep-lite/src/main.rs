70d63ab32509b02e2724c48e53128012aa552051
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-25 04:10:00 +0800

    automatically incrementing an index variable

--- p20-grep-lite/src/main.rs
+++ p20-grep-lite/src/main.rs
@@ -5,11 +5,10 @@ Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of pages?";
-    let mut line_num: usize = 1;
-    for line in quote.lines() {
+    for (i, line) in quote.lines().enumerate() {
         if line.contains(search_term) {
+            let line_num = i + 1;
             println!("{}: {}", line_num, line);
         }
-        line_num += 1;
     }
 }
