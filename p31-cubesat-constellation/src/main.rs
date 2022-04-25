3195a8606718b21c463686a5467bd3aa6c3e1cc3
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-25 21:01:55 +0800

    adding the groundstation.send() and cubeset.recv() methods

--- p31-cubesat-constellation/src/main.rs
+++ p31-cubesat-constellation/src/main.rs
@@ -1,5 +1,3 @@
-#![allow(unused_variables)]
-
 #[derive(Debug)]
 struct CubeSat {
     id: u64,
@@ -15,6 +13,18 @@ type Message = String;
 
 struct GroundStation;
 
+impl GroundStation {
+    fn send(&self, to: &mut CubeSat, msg: Message) {
+        to.mailbox.messages.push(msg);
+    }
+}
+
+impl CubeSat {
+    fn recv(&mut self) -> Option<Message> {
+        self.mailbox.messages.pop()
+    }
+}
+
 fn main() {
     let base = GroundStation {};
     let mut sat_a = CubeSat {
@@ -22,4 +32,9 @@ fn main() {
         mailbox: Mailbox { messages: vec![] },
     };
     println!("t0: {:?}", sat_a);
+    base.send(&mut sat_a, Message::from("hello there!"));
+    println!("t1: {:?}", sat_a);
+    let msg = sat_a.recv();
+    println!("t2: {:?}", sat_a);
+    println!("msg: {:?}", msg);
 }
