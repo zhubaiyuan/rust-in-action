75d95f6fd1cf6c0583f2604e48e5a00e0d2ca38f
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-25 18:42:23 +0800

    implement display for filestate, file

--- p26-represent-files/src/main.rs
+++ p26-represent-files/src/main.rs
@@ -1,3 +1,8 @@
+#![allow(dead_code)]
+
+use std::fmt;
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
 enum FileState {
     Open,
@@ -9,6 +14,21 @@ struct File {
     name: String,
     data: Vec<u8>,
     state: FileState,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
 }
 
 impl File {
@@ -43,16 +63,7 @@ fn close(mut f: File) -> Result<File, String> {
 }
 
 fn main() {
-    let mut f5 = File::new("5.txt");
-    let mut buffer: Vec<u8> = vec![];
-    if f5.read(&mut buffer).is_err() {
-        println!("Error checking is working");
-    }
-    f5 = open(f5).unwrap();
-    let f5_length = f5.read(&mut buffer).unwrap();
-    f5 = close(f5).unwrap();
-    let text = String::from_utf8_lossy(&buffer);
-    println!("{:?}", f5);
-    println!("{} is {} bytes long", &f5.name, f5_length);
-    println!("{}", text);
+    let f6 = File::new("f6.txt");
+    println!("{:?}", f6);
+    println!("{}", f6);
 }
