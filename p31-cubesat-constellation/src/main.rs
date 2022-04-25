223ec89e6ee6f8727ddaf5c9fda3ab2393c96475
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-25 20:42:19 +0800

    returning ownership back to the origin scope

--- p31-cubesat-constellation/src/main.rs
+++ p31-cubesat-constellation/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused_variables)]
+
 #[derive(Debug)]
 struct CubeSat {
     id: u64,
@@ -8,20 +10,19 @@ enum StatusMessage {
     Ok,
 }
 
-fn check_status(sat_id: CubeSat) -> StatusMessage {
-    StatusMessage::Ok
+fn check_status(sat_id: CubeSat) -> CubeSat {
+    println!("{:?}: {:?}", sat_id, StatusMessage::Ok);
+    sat_id
 }
 
 fn main() {
     let sat_a = CubeSat { id: 0 };
     let sat_b = CubeSat { id: 1 };
     let sat_c = CubeSat { id: 2 };
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
 }
