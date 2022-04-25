08babce9a87114caf23d4d0c1523f4f0aa96df3d
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-25 20:56:59 +0800

    adding a mailbox type to the system

--- p31-cubesat-constellation/src/main.rs
+++ p31-cubesat-constellation/src/main.rs
@@ -3,26 +3,23 @@
 #[derive(Debug)]
 struct CubeSat {
     id: u64,
+    mailbox: Mailbox,
 }
 
 #[derive(Debug)]
-enum StatusMessage {
-    Ok,
+struct Mailbox {
+    messages: Vec<Message>,
 }
 
-fn check_status(sat_id: CubeSat) -> CubeSat {
-    println!("{:?}: {:?}", sat_id, StatusMessage::Ok);
-    sat_id
-}
+type Message = String;
+
+struct GroundStation;
 
 fn main() {
-    let sat_a = CubeSat { id: 0 };
-    let sat_b = CubeSat { id: 1 };
-    let sat_c = CubeSat { id: 2 };
-    let sat_a = check_status(sat_a);
-    let sat_b = check_status(sat_b);
-    let sat_c = check_status(sat_c);
-    let sat_a = check_status(sat_a);
-    let sat_b = check_status(sat_b);
-    let sat_c = check_status(sat_c);
+    let base = GroundStation {};
+    let mut sat_a = CubeSat {
+        id: 0,
+        mailbox: Mailbox { messages: vec![] },
+    };
+    println!("t0: {:?}", sat_a);
 }
