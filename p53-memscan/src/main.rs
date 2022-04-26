d549d53bf27e3f05a45f09ed8a78c5f463672631
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-26 19:05:50 +0800

    scanning a process's memory

--- p53-memscan/src/main.rs
+++ p53-memscan/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let mut n_nonzero = 0;
-    for i in 0..10000 {
+    for i in 1..10000 {
         let ptr = i as *const u8;
         let byte_at_addr = unsafe { *ptr };
         if byte_at_addr != 0 {
