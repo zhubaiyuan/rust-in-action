abb025a4a4898ecdad3e8742bf54126c07fb4c81
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-27 14:58:03 +0800

    sending messages to and from a spawned thread

--- p79-channels/src/main.rs
+++ p79-channels/src/main.rs
@@ -4,12 +4,33 @@ extern crate crossbeam;
 use crossbeam::channel::unbounded;
 use std::thread;
 
+use crate::ConnectivityCheck::*;
+
+#[derive(Debug)]
+enum ConnectivityCheck {
+    Ping,
+    Pong,
+    Pang,
+}
+
 fn main() {
-    let (tx, rx) = unbounded();
-    thread::spawn(move || {
-        tx.send(42).unwrap();
+    let n_messages = 3;
+    let (requests_tx, requests_rx) = unbounded();
+    let (responses_tx, responses_rx) = unbounded();
+    thread::spawn(move || loop {
+        match requests_rx.recv().unwrap() {
+            Pong => eprintln!("unexpected pong response"),
+            Ping => responses_tx.send(Pong).unwrap(),
+            Pang => return,
+        }
     });
-    select! {
-       recv(rx) -> msg => println!("{:?}", msg),
+    for _ in 0..n_messages {
+        requests_tx.send(Ping).unwrap();
+    }
+    requests_tx.send(Pang).unwrap();
+    for _ in 0..n_messages {
+        select! {
+           recv(responses_rx) -> msg => println!("{:?}", msg),
+        }
     }
 }
