04d1a9953c46b1aa1f0a587ccea75d264289cb4c
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-27 13:37:51 +0800

    producing formatted dates from the command line

--- p70-clock/src/main.rs
+++ p70-clock/src/main.rs
@@ -1,6 +1,53 @@
+use chrono::DateTime;
 use chrono::Local;
+use clap::{Arg, Command};
+
+struct Clock;
+
+impl Clock {
+    fn get() -> DateTime<Local> {
+        Local::now()
+    }
+
+    #[allow(dead_code)]
+    fn set() -> ! {
+        unimplemented!()
+    }
+}
 
 fn main() {
-    let now = Local::now();
-    println!("{}", now);
+    let app = Command::new("clock")
+        .version("0.1")
+        .about("Gets and (aspirationally) sets the time.")
+        .arg(
+            Arg::new("action")
+                .takes_value(true)
+                .possible_values(&["get", "set"])
+                .default_value("get"),
+        )
+        .arg(
+            Arg::new("std")
+                .short('s')
+                .long("use-standard")
+                .takes_value(true)
+                .possible_values(&["rfc2822", "rfc3339", "timestamp"])
+                .default_value("rfc3339"),
+        )
+        .arg(Arg::new("datetime").help(
+            "When <action> is 'set', apply <datetime>. \
+       Otherwise, ignore.",
+        ));
+    let args = app.get_matches();
+    let action = args.value_of("action").unwrap();
+    let std = args.value_of("std").unwrap();
+    if action == "set" {
+        unimplemented!()
+    }
+    let now = Clock::get();
+    match std {
+        "timestamp" => println!("{}", now.timestamp()),
+        "rfc2822" => println!("{}", now.to_rfc2822()),
+        "rfc3339" => println!("{}", now.to_rfc3339()),
+        _ => unreachable!(),
+    }
 }
