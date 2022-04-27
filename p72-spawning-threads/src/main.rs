17adf01ca54ee9d12bf32d45a1dc837fe84afb8e
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-27 14:12:54 +0800

    creating two subthreads to perform work on our behalf

--- p72-spawning-threads/src/main.rs
+++ p72-spawning-threads/src/main.rs
@@ -2,11 +2,16 @@ use std::{thread, time};
 
 fn main() {
     let start = time::Instant::now();
-    let handler = thread::spawn(|| {
-        let pause = time::Duration::from_millis(300);
+    let handler_1 = thread::spawn(|| {
+        let pause = time::Duration::from_millis(3000);
         thread::sleep(pause.clone());
     });
-    handler.join().unwrap();
+    let handler_2 = thread::spawn(|| {
+        let pause = time::Duration::from_millis(3000);
+        thread::sleep(pause.clone());
+    });
+    handler_1.join().unwrap();
+    handler_2.join().unwrap();
     let finish = time::Instant::now();
     println!("{:02?}", finish.duration_since(start));
 }
