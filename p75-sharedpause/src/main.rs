35c664225ff4dffcf86110cb26f4d2b17c82eb2a
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-27 14:31:06 +0800

    using a variable defined in a parent scope in multiple closures

--- p75-sharedpause/src/main.rs
+++ p75-sharedpause/src/main.rs
@@ -1,11 +1,12 @@
 use std::{thread, time};
 
+#[allow(unused_must_use)]
 fn main() {
     let pause = time::Duration::from_millis(20);
-    let handle1 = thread::spawn(|| {
+    let handle1 = thread::spawn(move || {
         thread::sleep(pause);
     });
-    let handle2 = thread::spawn(|| {
+    let handle2 = thread::spawn(move || {
         thread::sleep(pause);
     });
     handle1.join();
